a696a3c403b1eae8153f78c92491551aaa2152b8
Qun Cheng <quncheng@udel.edu>
2018-04-15 14:39:25 -0400

    editing README

--- src/ButtonState.java
+++ src/ButtonState.java
@@ -7,32 +7,34 @@ import java.awt.Image;
  If a button shows a mine, use danger icon.
  If there's no mine surrounding, use safe icon.
  Otherwise, show the number of the number of adjacent mines surrounding a certain tile.
-*/ 
+ */
 public class ButtonState extends JButton{
-	// Buttons should get images set up
-	// D means danger, S means safe, the number Icons(ONE, TWO, ..., EIGHT) stands for the number of adjacent mines.
-	ImageIcon D, S, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT; 
-
-	public ButtonState() {
-		// Icon website: https://www.flaticon.com/search?word=number
-		D = scaleImage(new ImageIcon("DangerIcon1.png"));
-		S = scaleImage(new ImageIcon("SafeIcon.png"));
-		ONE = scaleImage(new ImageIcon("1.png"));
-		TWO = scaleImage(new ImageIcon("2.png"));
-		THREE = scaleImage(new ImageIcon("3.png"));
-		FOUR = scaleImage(new ImageIcon("4.png"));
-		FIVE = scaleImage(new ImageIcon("5.png"));
-		SIX = scaleImage(new ImageIcon("6.png"));
-		SEVEN = scaleImage(new ImageIcon("7.png"));
-		EIGHT = scaleImage(new ImageIcon("8.png"));
-	}	
-
-	// Scale images to fit cubes
-	// How to scale images: https://stackoverflow.com/questions/13810213/java-stretch-icon-to-fit-button
-	public ImageIcon scaleImage(ImageIcon imgIcon) {
-		int iconSize = (int) (Minesweeper.FRAME_SIZE/(1.2 * Minesweeper.buttonNumRow));
-		Image newImage = imgIcon.getImage().getScaledInstance(iconSize, iconSize, java.awt.Image.SCALE_SMOOTH);
-		return new ImageIcon(newImage);
-	}
+    // Buttons should get images set up
+    // D means danger, S means safe, the number Icons(ONE, TWO, ..., EIGHT) stands for the number of adjacent mines.
+    ImageIcon D, S, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT;
+    
+    public ButtonState() {
+        // Icon website: https://www.flaticon.com/search?word=number
+        D = scaleImage(new ImageIcon("DangerIcon1.png"));
+        S = scaleImage(new ImageIcon("SafeIcon.png"));
+        ONE = scaleImage(new ImageIcon("1.png"));
+        TWO = scaleImage(new ImageIcon("2.png"));
+        THREE = scaleImage(new ImageIcon("3.png"));
+        FOUR = scaleImage(new ImageIcon("4.png"));
+        FIVE = scaleImage(new ImageIcon("5.png"));
+        SIX = scaleImage(new ImageIcon("6.png"));
+        SEVEN = scaleImage(new ImageIcon("7.png"));
+        EIGHT = scaleImage(new ImageIcon("8.png"));
+    }
+    
+    // Scale images to fit cubes
+    // How to scale images: https://stackoverflow.com/questions/13810213/java-stretch-icon-to-fit-button
+    public ImageIcon scaleImage(ImageIcon imgIcon) {
+        int iconSize = (int) (Minesweeper.FRAME_SIZE/(1.2 * Minesweeper.buttonNumRow));
+        Image newImage = imgIcon.getImage().getScaledInstance(iconSize, iconSize, java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(newImage);
+    }
 }
+
+
 
--- src/Game.java
+++ src/Game.java
@@ -3,80 +3,81 @@ import java.util.Scanner;
 /*
  The Game class is used to run this Game.
  It provides users two modes for this game.
- Default mode customMode in which a user can specify the mine field dimensions, i.e. how many tiles per row 
+ Default mode customMode in which a user can specify the mine field dimensions, i.e. how many tiles per row
  and how many tiles per column
  Classic mode specifies three levels for users. In this mode, they can choose different Game levels(Easy, Medium, Hard)
  */
 public class Game {
-	public static void main(String args[]) {
-		if (args.length > 0 && args[0].equals("classic")) {
-			startGameClassic();
-		} else {
-			startGame();
-		}
-	}
-	
-	// Set game as customMode. Users can customize their own grid(rows x columns).
-	public static void startGame() {
-		
-		// Input row and columns for grid.
-		System.out.println("How many rows do you want?");
-		Scanner input = new Scanner(System.in);  // Reading from System.in
-		int row = (int)input.nextInt();  
-		
-		System.out.println("How many columns do you want?");
-		int col = input.nextInt();
-		input.close();
-		
-		// Deal with invalid input. If any input number is less than or equal to 0, print error information
-		if (row <= 0) {
-			System.out.println("Invalid input!");
-			return;
-		}
-		if (col <= 0) {
-			System.out.println("Invalid input!");
-			return;
-		} 
-
-		Minesweeper.buttonNumRow = row;
-		Minesweeper.buttonNumCol = col;
-		Minesweeper.MINES_NUM = (row * col) / 4;
-		new Minesweeper();
-	}
-
-	// Set game as classicMode. Users can choose three difficulties: Easy, Medium, and Hard.
-	public static void startGameClassic() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter a level:");
-		System.out.println("0 -- Easy\n1 -- Medium\n2 -- Hard");
-		int level = (int)input.nextInt();
-		
-		// Different game level based on user's input.
-		// 0 represents easy,
-		// 1 represents medium,
-		// 2 represents hard.
-		// otherwise print error information
-		switch(level) {
-		case 0:
-			Minesweeper.buttonNumRow = 5;
-			Minesweeper.buttonNumCol = 5;
-			Minesweeper.MINES_NUM = 7;
-			new Minesweeper();
-			break;
-		case 1:
-			Minesweeper.buttonNumRow = 10;
-			Minesweeper.buttonNumCol = 10;
-			Minesweeper.MINES_NUM = 30;
-			new Minesweeper();
-			break;
-		case 2:
-			Minesweeper.buttonNumRow = 15;
-			Minesweeper.buttonNumCol = 15;
-			Minesweeper.MINES_NUM = 35;
-			new Minesweeper();
-			break;
-		default:
-			System.out.println("Invalid input!");
-		}
-	}
+    public static void main(String args[]) {
+        if (args.length > 0 && args[0].equals("classic")) {
+            startGameClassic();
+        } else {
+            startGame();
+        }
+    }
+    
+    // Set game as customMode. Users can customize their own grid(rows x columns).
+    public static void startGame() {
+        
+        // Input row and columns for grid.
+        System.out.println("How many rows do you want?");
+        Scanner input = new Scanner(System.in);  // Reading from System.in
+        int row = (int)input.nextInt();
+        
+        System.out.println("How many columns do you want?");
+        int col = input.nextInt();
+        input.close();
+        
+        // Deal with invalid input. If any input number is less than or equal to 0, print error information
+        if (row <= 0) {
+            System.out.println("Invalid input!");
+            return;
+        }
+        if (col <= 0) {
+            System.out.println("Invalid input!");
+            return;
+        }
+        
+        Minesweeper.buttonNumRow = row;
+        Minesweeper.buttonNumCol = col;
+        Minesweeper.MINES_NUM = (row * col) / 4;
+        new Minesweeper();
+    }
+    
+    // Set game as classicMode. Users can choose three difficulties: Easy, Medium, and Hard.
+    public static void startGameClassic() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a level:");
+        System.out.println("0 -- Easy\n1 -- Medium\n2 -- Hard");
+        int level = (int)input.nextInt();
+        
+        // Different game level based on user's input.
+        // 0 represents easy,
+        // 1 represents medium,
+        // 2 represents hard.
+        // otherwise print error information
+        switch(level) {
+            case 0:
+                Minesweeper.buttonNumRow = 5;
+                Minesweeper.buttonNumCol = 5;
+                Minesweeper.MINES_NUM = 7;
+                new Minesweeper();
+                break;
+            case 1:
+                Minesweeper.buttonNumRow = 10;
+                Minesweeper.buttonNumCol = 10;
+                Minesweeper.MINES_NUM = 30;
+                new Minesweeper();
+                break;
+            case 2:
+                Minesweeper.buttonNumRow = 15;
+                Minesweeper.buttonNumCol = 15;
+                Minesweeper.MINES_NUM = 35;
+                new Minesweeper();
+                break;
+            default:
+                System.out.println("Invalid input!");
+                return;
+        }
+    }
 }
